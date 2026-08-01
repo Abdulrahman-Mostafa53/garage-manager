8837abbb70ce72ae22564eb0f6b948efbcca0b12
Abdulrahman Mostafa <abdulrahmanmostafa1101@gmail.com>
2026-08-01 23:28:36 +0300

    add gui

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_garage_FILE D:/programming/c++/Task 4/build/garage.exe)
+set(__QT_DEPLOY_TARGET_garage_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_garage_RUNTIME_DLLS D:/Programs/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;D:/Programs/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;D:/Programs/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
---
+++ build/garage_autogen/EWIEGA46WW/moc_gui.cpp
@@ -0,0 +1,149 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'gui.h'
+**
+** Created by: The Qt Meta Object Compiler version 69 (Qt 6.11.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../gui.h"
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'gui.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 69
+#error "This file was generated using the moc from 6.11.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+struct qt_meta_tag_ZN3GuiE_t {};
+} // unnamed namespace
+
+template <> constexpr inline auto Gui::qt_create_metaobjectdata<qt_meta_tag_ZN3GuiE_t>()
+{
+    namespace QMC = QtMocConstants;
+    QtMocHelpers::StringRefStorage qt_stringData {
+        "Gui",
+        "on_checkInBut_clicked",
+        "",
+        "on_viewGarageBut_clicked",
+        "on_searchAndTuneBut_clicked",
+        "on_retirePanel_clicked",
+        "on_checkIn_clicked",
+        "on_searchBut_clicked",
+        "on_typeC_currentTextChanged",
+        "arg1",
+        "on_editButton_clicked",
+        "on_typeE_currentTextChanged",
+        "on_editNowBut_clicked"
+    };
+
+    QtMocHelpers::UintData qt_methods {
+        // Slot 'on_checkInBut_clicked'
+        QtMocHelpers::SlotData<void()>(1, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'on_viewGarageBut_clicked'
+        QtMocHelpers::SlotData<void()>(3, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'on_searchAndTuneBut_clicked'
+        QtMocHelpers::SlotData<void()>(4, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'on_retirePanel_clicked'
+        QtMocHelpers::SlotData<void()>(5, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'on_checkIn_clicked'
+        QtMocHelpers::SlotData<void()>(6, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'on_searchBut_clicked'
+        QtMocHelpers::SlotData<void()>(7, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'on_typeC_currentTextChanged'
+        QtMocHelpers::SlotData<void(const QString &)>(8, 2, QMC::AccessPrivate, QMetaType::Void, {{
+            { QMetaType::QString, 9 },
+        }}),
+        // Slot 'on_editButton_clicked'
+        QtMocHelpers::SlotData<void()>(10, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'on_typeE_currentTextChanged'
+        QtMocHelpers::SlotData<void(const QString &)>(11, 2, QMC::AccessPrivate, QMetaType::Void, {{
+            { QMetaType::QString, 9 },
+        }}),
+        // Slot 'on_editNowBut_clicked'
+        QtMocHelpers::SlotData<void()>(12, 2, QMC::AccessPrivate, QMetaType::Void),
+    };
+    QtMocHelpers::UintData qt_properties {
+    };
+    QtMocHelpers::UintData qt_enums {
+    };
+    return QtMocHelpers::metaObjectData<Gui, qt_meta_tag_ZN3GuiE_t>(QMC::MetaObjectFlag{}, qt_stringData,
+            qt_methods, qt_properties, qt_enums);
+}
+Q_CONSTINIT const QMetaObject Gui::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_staticMetaObjectStaticContent<qt_meta_tag_ZN3GuiE_t>.stringdata,
+    qt_staticMetaObjectStaticContent<qt_meta_tag_ZN3GuiE_t>.data,
+    qt_static_metacall,
+    nullptr,
+    qt_staticMetaObjectRelocatingContent<qt_meta_tag_ZN3GuiE_t>.metaTypes,
+    nullptr
+} };
+
+void Gui::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    auto *_t = static_cast<Gui *>(_o);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        switch (_id) {
+        case 0: _t->on_checkInBut_clicked(); break;
+        case 1: _t->on_viewGarageBut_clicked(); break;
+        case 2: _t->on_searchAndTuneBut_clicked(); break;
+        case 3: _t->on_retirePanel_clicked(); break;
+        case 4: _t->on_checkIn_clicked(); break;
+        case 5: _t->on_searchBut_clicked(); break;
+        case 6: _t->on_typeC_currentTextChanged((*reinterpret_cast<std::add_pointer_t<QString>>(_a[1]))); break;
+        case 7: _t->on_editButton_clicked(); break;
+        case 8: _t->on_typeE_currentTextChanged((*reinterpret_cast<std::add_pointer_t<QString>>(_a[1]))); break;
+        case 9: _t->on_editNowBut_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *Gui::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Gui::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_staticMetaObjectStaticContent<qt_meta_tag_ZN3GuiE_t>.strings))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int Gui::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 10)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 10;
+    }
+    if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 10)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 10;
+    }
+    return _id;
+}
+QT_WARNING_POP
